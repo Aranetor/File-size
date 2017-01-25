2eff6198b354bd6a67bbff309ebb402a77293db1
Aranetor <p.aranetor@gmail.com>
2017-01-25 22:29:58 +0100

    heroku debug

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,8 +4,7 @@ const multer = require('multer');
 module.exports = function(app) {
   app.get('/', (req, res) => {
     // You'll create your note here
-    //res.sendFile(path.join(__dirname+'/index.html'));
-    console.log(path.join(__dirname+'/index.html'));
+    res.sendFile(path.join(__dirname+'/index.html'));
   });
 
   app.post('/get-file-size', multer().single('file'), (req, res) => {
--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const port          = 3000;
 
 require('./app/routes')(app);
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log('We are live on ' + port);
 });
