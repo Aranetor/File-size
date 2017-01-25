4e84c87003b6ad68f6d54146e4eacb890d3d89ab
Aranetor <p.aranetor@gmail.com>
2017-01-25 22:19:56 +0100

    working

---
+++ app/routes/routes.js
@@ -0,0 +1,13 @@
+const path   = require('path');
+const multer = require('multer');
+
+module.exports = function(app) {
+  app.get('/', (req, res) => {
+    // You'll create your note here
+    res.sendFile(path.join(__dirname+'/index.html'));
+  });
+
+  app.post('/get-file-size', multer().single('file'), (req, res) => {
+    res.send({"size":req.file.size});
+  });
+};
